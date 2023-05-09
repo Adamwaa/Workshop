c72ba4711f09f19c3c939617c624de80734c360e
Adam Wrutniak <adamwrutniak@gmail.com>
2023-05-09 13:33:33 +0200

    V0.4: Fixing RegistrationServiceTest.

--- src/test/java/thymeleaf/Workshop/RegistrationControllerTest.java
+++ src/test/java/thymeleaf/Workshop/RegistrationControllerTest.java
@@ -1,17 +1,31 @@
 package thymeleaf.Workshop;
 
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.Mockito.*;
 
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 import thymeleaf.Workshop.controller.RegistrationController;
 import thymeleaf.Workshop.model.Registration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 public class RegistrationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void testAddRegistration() {
@@ -28,4 +42,9 @@ public class RegistrationControllerTest {
     }
 
 
+    @Test
+    public void testGreeting() throws Exception {
+        this.mockMvc.perform(get("/greeting"))
+                .andExpect(status().isOk());
+    }
 }
